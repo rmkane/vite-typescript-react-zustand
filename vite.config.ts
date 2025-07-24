fce02530d0c770ec048e98e7ed883b711cabdb77
rmkane <rmkane@proton.me>
2025-07-24 08:53:30 -0400

    Simplify aliases with vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,11 @@
 /// <reference types="vitest" />
-import path from 'node:path'
-
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@/': path.resolve(__dirname, 'src'),
-      '@/assets': path.resolve(__dirname, 'src/assets'),
-      '@/components': path.resolve(__dirname, 'src/components'),
-      '@/contexts': path.resolve(__dirname, 'src/contexts'),
-      '@/hooks': path.resolve(__dirname, 'src/hooks'),
-      '@/store': path.resolve(__dirname, 'src/store'),
-      '@/utils': path.resolve(__dirname, 'src/utils'),
-    },
-  },
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
